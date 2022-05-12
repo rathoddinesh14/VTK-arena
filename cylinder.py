575d8efb9a37040d90cca1f62d443a86c0658bc1
Rathod <dinesh.rathod.c2019@iitbombay.org>
2022-05-12 19:38:27 +0530

    sample code

---
+++ cylinder.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# pipeline creation and rendering for cylinder
+
+import vtkmodules.vtkInteractionStyle
+import vtkmodules.vtkRenderingOpenGL2
+from vtkmodules.vtkCommonColor import vtkNamedColors
+from vtkmodules.vtkFiltersSources import vtkCylinderSource
+from vtkmodules.vtkRenderingCore import (
+    vtkActor,
+    vtkPolyDataMapper,
+    vtkRenderWindow,
+    vtkRenderWindowInteractor,
+    vtkRenderer
+)
+
+def main():
+    colors = vtkNamedColors()
