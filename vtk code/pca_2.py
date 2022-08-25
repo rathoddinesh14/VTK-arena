5d202679d5c992a74e5b9630f008b3b55304299c
64bitmachine <85003035+rathoddinesh14@users.noreply.github.com>
2022-08-25 21:36:57 +0530

    refactored code

--- vtk code/pca_2.py
+++ vtk code/pca_2.py
@@ -1,4 +1,97 @@
 import vtk
+import random
+
+def get_eigen_axis_actor(center, eigen_value, eigen_vector, color):
+    """
+    Get the eigen axis passing through the center
+    @param center: center
+    @param eigen_value: eigen value of the eigen vector
+    @param eigen_vector: eigen vector (x, y, z)
+    @param color: color of the eigen axis
+    """
+    axis = vtk.vtkLineSource()
+    axis.SetPoint1([    center[0] + eigen_value * eigen_vector[0], 
+                        center[1] + eigen_value * eigen_vector[1],
+                        center[2] + eigen_value * eigen_vector[2]])
+    axis.SetPoint2([    center[0] - eigen_value * eigen_vector[0],
+                        center[1] - eigen_value * eigen_vector[1],
+                        center[2] - eigen_value * eigen_vector[2]])
+    axis.Update()
+
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(axis.GetOutputPort())
+    mapper.Update()
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    actor.GetProperty().SetColor(color)
+
+    return actor
+
+
+def get_eigen_vectors_values(points):
+    '''
+    returns eigen values, vector
+    @param points: list of points (x, y, z)
+    @return: eigen values, vector
+    '''
+    # vtk double array
+    xArr = vtk.vtkDoubleArray()
+    xArr.SetName("x")
+    xArr.SetNumberOfComponents(1)
+
+    yArr = vtk.vtkDoubleArray()
+    yArr.SetName("y")
+    yArr.SetNumberOfComponents(1)
+
+    zArr = vtk.vtkDoubleArray()
+    zArr.SetName("z")
+    zArr.SetNumberOfComponents(1)
+
+    for i in range(points.GetNumberOfPoints()):
+        xArr.InsertNextValue(points.GetPoint(i)[0])
+        yArr.InsertNextValue(points.GetPoint(i)[1])
+        zArr.InsertNextValue(points.GetPoint(i)[2])
+
+    # vtk table
+    table = vtk.vtkTable()
+    table.AddColumn(xArr)
+    table.AddColumn(yArr)
+    table.AddColumn(zArr)
+
+    # vtk pca
+    pca = vtk.vtkPCAStatistics()
+    pca.SetInputData(table)
+    pca.SetColumnStatus("x", 1)
+    pca.SetColumnStatus("y", 1)
+    pca.SetColumnStatus("z", 1)
+    pca.RequestSelectedColumns()
+    pca.SetDeriveOption(True)
+    pca.Update()
+
+    # eigenvalues
+    eigenvalues = vtk.vtkDoubleArray()
+    pca.GetEigenvalues(eigenvalues)
+    ev = []
+    # print("Eigenvalues: ")
+    for i in range(eigenvalues.GetNumberOfTuples()):
+        # print(eigenvalues.GetValue(i))
+        ev.append(eigenvalues.GetValue(i))
+
+    # eigenvectors
+    eigenvectors = vtk.vtkDoubleArray()
+    pca.GetEigenvectors(eigenvectors)
+
+    eig_vec = [vtk.vtkDoubleArray() for i in range(eigenvectors.GetNumberOfTuples())]
+    for i in range(eigenvectors.GetNumberOfTuples()):
+        pca.GetEigenvector(i, eig_vec[i])
+
+    eig_vec_2 = []
+    for i in range(len(eig_vec)):
+        eig_vec_2.append((eig_vec[i].GetValue(0), eig_vec[i].GetValue(1), eig_vec[i].GetValue(2)))
+
+    return eig_vec_2, ev
+
 
 points = vtk.vtkPoints()
 points.SetNumberOfPoints(200)
@@ -7,154 +100,41 @@ boxMueller = vtk.vtkBoxMuellerRandomSequence()
 
 centre = [0.0, 0.0, 0.0]
 
-for i in range(200):
+# populate points
+for i in range(points.GetNumberOfPoints()):
     # random point in x in [0, 3], y in [0, 5], z in [0, 2]
     # scaled value
-    x = boxMueller.GetScaledValue(0, 3)
-    boxMueller.Next()
-    y = boxMueller.GetScaledValue(0, 5)
-    boxMueller.Next()
-    z = boxMueller.GetScaledValue(0, 2)
-    boxMueller.Next()
+    # x = boxMueller.GetScaledValue(0, 3)
+    # boxMueller.Next()
+    # y = boxMueller.GetScaledValue(0, 5)
+    # boxMueller.Next()
+    # z = boxMueller.GetScaledValue(0, 2)
+    # boxMueller.Next()
+    # sample from sphere
+    x = random.random() * 4
+    y = random.random() * 5
+    z = random.random() * 2
     points.SetPoint(i, x, y, z)
     cellArray.InsertNextCell(1)
     cellArray.InsertCellPoint(i)
-    print(x, y, z)
+    # print(x, y, z)
     centre[0] += x
     centre[1] += y
     centre[2] += z
 
-centre[0] /= 200
-centre[1] /= 200
-centre[2] /= 200
+centre[0] /= points.GetNumberOfPoints()
+centre[1] /= points.GetNumberOfPoints()
+centre[2] /= points.GetNumberOfPoints()
 
 # Create a polydata object
 polydata = vtk.vtkPolyData()
 polydata.SetPoints(points)
 polydata.SetVerts(cellArray)
 
-# vtk double array
-xArr = vtk.vtkDoubleArray()
-xArr.SetName("x")
-xArr.SetNumberOfComponents(1)
+eigenvectors, eigenvalues = get_eigen_vectors_values(points)
 
-yArr = vtk.vtkDoubleArray()
-yArr.SetName("y")
-yArr.SetNumberOfComponents(1)
-
-zArr = vtk.vtkDoubleArray()
-zArr.SetName("z")
-zArr.SetNumberOfComponents(1)
-
-for i in range(200):
-    xArr.InsertNextValue(polydata.GetPoint(i)[0])
-    yArr.InsertNextValue(polydata.GetPoint(i)[1])
-    zArr.InsertNextValue(polydata.GetPoint(i)[2])
-
-# vtk table
-table = vtk.vtkTable()
-table.AddColumn(xArr)
-table.AddColumn(yArr)
-table.AddColumn(zArr)
-
-# vtk pca
-pca = vtk.vtkPCAStatistics()
-pca.SetInputData(table)
-pca.SetColumnStatus("x", 1)
-pca.SetColumnStatus("y", 1)
-pca.SetColumnStatus("z", 1)
-pca.RequestSelectedColumns()
-pca.SetDeriveOption(True)
-pca.Update()
-
-# eigenvalues
-eigenvalues = vtk.vtkDoubleArray()
-pca.GetEigenvalues(eigenvalues)
-print("Eigenvalues: ")
-for i in range(eigenvalues.GetNumberOfTuples()):
-    print(eigenvalues.GetValue(i))
-
-# eigenvectors
-eigenvectors = vtk.vtkDoubleArray()
-pca.GetEigenvectors(eigenvectors)
-# print("Eigenvectors: ")
-# for i in range(eigenvectors.GetNumberOfTuples()):
-#     print(eigenvectors.GetValue(i))
-
-eig_vec = [vtk.vtkDoubleArray() for i in range(eigenvectors.GetNumberOfTuples())]
-for i in range(eigenvectors.GetNumberOfTuples()):
-    pca.GetEigenvector(i, eig_vec[i])
-
-print("Eigenvectors: ")
-for i in range(eigenvectors.GetNumberOfTuples()):
-    print(eig_vec[i].GetValue(0), eig_vec[i].GetValue(1), eig_vec[i].GetValue(2))
-
-# end points of eigenvectors
-ev_1_end_1 = [0.0, 0.0, 0.0]
-ev_1_end_2 = [0.0, 0.0, 0.0]
-# calculate end points of eigenvectors
-for i in range(0, 3):
-    ev_1_end_1[i] = centre[i] + eig_vec[0].GetValue(i) * eigenvalues.GetValue(0) / 2
-    ev_1_end_2[i] = centre[i] - eig_vec[0].GetValue(i) * eigenvalues.GetValue(0) / 2
-
-ev_2_end_1 = [0.0, 0.0, 0.0]
-ev_2_end_2 = [0.0, 0.0, 0.0]
-for i in range(0, 3):
-    ev_2_end_1[i] = centre[i] + eig_vec[1].GetValue(i) * eigenvalues.GetValue(1) / 2
-    ev_2_end_2[i] = centre[i] - eig_vec[1].GetValue(i) * eigenvalues.GetValue(1) / 2
-
-ev_3_end_1 = [0.0, 0.0, 0.0]
-ev_3_end_2 = [0.0, 0.0, 0.0]
-for i in range(0, 3):
-    ev_3_end_1[i] = centre[i] + eig_vec[2].GetValue(i) * eigenvalues.GetValue(2) / 2
-    ev_3_end_2[i] = centre[i] - eig_vec[2].GetValue(i) * eigenvalues.GetValue(2) / 2
-
-print("End points of eigenvector 1: ")
-print(ev_1_end_1[0], ev_1_end_1[1], ev_1_end_1[2])
-print(ev_1_end_2[0], ev_1_end_2[1], ev_1_end_2[2])
-
-# line source
-lineSource = vtk.vtkLineSource()
-lineSource.SetPoint1(ev_1_end_1)
-lineSource.SetPoint2(ev_1_end_2)
-
-# mapper
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputConnection(lineSource.GetOutputPort())
-
-# actor
-lactor = vtk.vtkActor()
-lactor.SetMapper(mapper)
-lactor.GetProperty().SetColor(1.0, 0.0, 0.0)
-
-# line source 2
-lineSource2 = vtk.vtkLineSource()
-lineSource2.SetPoint1(ev_2_end_1)
-lineSource2.SetPoint2(ev_2_end_2)
-
-# mapper 2
-mapper2 = vtk.vtkPolyDataMapper()
-mapper2.SetInputConnection(lineSource2.GetOutputPort())
-
-# actor 2
-lactor2 = vtk.vtkActor()
-lactor2.SetMapper(mapper2)
-lactor2.GetProperty().SetColor(0.0, 1.0, 0.0)
-
-# line source 3
-lineSource3 = vtk.vtkLineSource()
-lineSource3.SetPoint1(ev_3_end_1)
-lineSource3.SetPoint2(ev_3_end_2)
-
-# mapper 3
-mapper3 = vtk.vtkPolyDataMapper()
-mapper3.SetInputConnection(lineSource3.GetOutputPort())
-
-# actor 3
-lactor3 = vtk.vtkActor()
-lactor3.SetMapper(mapper3)
-lactor3.GetProperty().SetColor(0.0, 0.0, 1.0)
-
+print(eigenvectors)
+print(eigenvalues)
 
 # sphere at centre
 sphere = vtk.vtkSphereSource()
@@ -187,10 +167,10 @@ renderWindowInteractor.SetRenderWindow(renderWindow)
 
 # Add the actor to the scene
 renderer.AddActor(actor)
-renderer.AddActor(sactor)
-renderer.AddActor(lactor)
-renderer.AddActor(lactor2)
-renderer.AddActor(lactor3)
+# renderer.AddActor(sactor)
+renderer.AddActor(get_eigen_axis_actor(centre, eigenvalues[0], eigenvectors[0], (1, 0, 0)))
+renderer.AddActor(get_eigen_axis_actor(centre, eigenvalues[1], eigenvectors[1], (0, 1, 0)))
+renderer.AddActor(get_eigen_axis_actor(centre, eigenvalues[2], eigenvectors[2], (0, 0, 1)))
 renderer.SetBackground(0.1, 0.2, 0.3) # Background color white
 
 # Render and interact
